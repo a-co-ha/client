5f54d4dabb1df998e8ba16a9ce37dddea47caa00
ghkd1418 <1004wipi@gmail.com>
2023-03-21 14:55:05 +0900

    feat: QueryErrorResetBoundary를 통한 바운더리 안 api 재호출 구현

--- src/components/error-boundary/Error.tsx
+++ src/components/error-boundary/Error.tsx
@@ -1,3 +1,17 @@
-export const Error = () => {
-  return <>editable page 에러컴포넌트</>;
+interface ErrorProps {
+  onReset: () => void;
+}
+
+export const Error = ({ onReset }: ErrorProps) => {
+  return (
+    <>
+      <button
+        onClick={() => {
+          onReset();
+        }}
+      >
+        editable page 에러컴포넌트
+      </button>
+    </>
+  );
 };
--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 interface Props {
   children: React.ReactNode;
   fallback: React.ElementType;
+  onReset: () => void;
 }
 
 interface State {
@@ -10,10 +11,15 @@ interface State {
   info: Error | null;
 }
 
+const initialState: State = {
+  hasError: false,
+  info: null,
+};
+
 export class ErrorBoundary extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { hasError: false, info: null };
+    this.state = initialState;
   }
 
   static getDerivedStateFromError(error: Error) {
@@ -24,10 +30,25 @@ export class ErrorBoundary extends React.Component<Props, State> {
     console.log('error', error, 'errorInfo', errorInfo);
   }
 
+  onResetErrorBoundary = () => {
+    const { onReset } = this.props;
+    onReset == null ? void 0 : this.props.onReset();
+    this.reset();
+  };
+
+  reset() {
+    this.setState(initialState);
+  }
+
   render() {
     if (this.state.hasError) {
       // 폴백 UI를 커스텀하여 렌더링할 수 있습니다.
-      return <this.props.fallback error={this.state.info} />;
+      return (
+        <this.props.fallback
+          onError={this.state.info}
+          onReset={this.onResetErrorBoundary}
+        />
+      );
     }
     return this.props.children;
   }
