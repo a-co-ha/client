885d7300abf26c738e62d62b2f5e9015271f9d0e
tangjinlog <lkg70007@gmail.com>
2023-03-29 00:53:11 +0900

    refactor: editable-select-page mutation에 switch case구문으로 수정

--- src/hooks/queries/editable/postPage.ts
+++ src/hooks/queries/editable/postPage.ts
@@ -2,19 +2,24 @@ import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { postEditablePage } from '@/pages/api/editable';
 import type { AxiosError } from 'axios';
 import type { PostEditablePage } from '@/pages/api/editable/type';
+import { useRouter } from 'next/router';
 
 export const usePostEditablePage = (
   channelId: string | string[] | undefined
 ) => {
   console.log('채널 아이디이이', channelId);
   const queryClient = useQueryClient();
+  const router = useRouter();
   return useMutation<PostEditablePage, AxiosError>(
     () => postEditablePage(channelId),
     {
-      onSuccess: () => {
+      onSuccess: (data) => {
         if (channelId) {
-          console.log(`석세스`);
+          console.log(`석세스`, data);
           queryClient.invalidateQueries([`editablePages`, channelId]);
+          router.push(
+            `/project/${channelId}/${data._id}?name=${data.pageName}&type=${data.type}`
+          );
         }
       },
     }
