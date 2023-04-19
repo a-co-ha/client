127311d27c470265ff75d5f525a8c58f74a0fea9
tangjinlog <lkg70007@gmail.com>
2023-04-19 16:20:25 +0900

    chore: inviteUser accessToken 예외처리 추가

--- src/pages/api/user/inviteUser.ts
+++ src/pages/api/user/inviteUser.ts
@@ -1,9 +1,13 @@
 import { api } from '../config/api-config';
+import { getCookie } from 'cookies-next';
 
 export const inviteUser = async (
   adminCode: string | string[] | undefined,
   channelName: string | string[] | undefined
 ) => {
+  const accessToken = getCookie(`accessToken`);
+  if (!accessToken) window.location.href = `/error`;
+
   const res = await api.post(`/invite/${adminCode}?channelCode=${channelName}`);
   return res.data;
 };
