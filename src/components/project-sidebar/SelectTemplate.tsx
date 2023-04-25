8257bad0d23ed9d04d45ddfb13e9554737e986fb
ghkd1418 <1004wipi@gmail.com>
2023-04-26 00:00:18 +0900

    chore: popover 생성 위치 조정

--- src/components/project-sidebar/SelectTemplate.tsx
+++ src/components/project-sidebar/SelectTemplate.tsx
@@ -60,7 +60,7 @@ export const SelectTemplate = ({ pageType }: { pageType: string }) => {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute left-1/5 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+        <Popover.Panel className="absolute left-1/5 z-10 mt-3 w-screen max-w-sm -translate-x-1/5 transform px-4 sm:px-0 lg:max-w-3xl">
           {({ close }) => (
             <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
