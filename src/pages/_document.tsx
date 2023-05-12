bb823abc74fdee524ce7a38937b860c2ba731c7e
tangjinlog <lkg70007@gmail.com>
2023-05-12 17:36:07 +0900

    feat: _document.tsx에 meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import acohaImage from '@/images/channelImg/1.png';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content={`아코하`} key="og-title" />
+        <meta
+          property="og:description"
+          content={`아코하입니다`}
+          key="og-desc"
+        />
+        <meta property="og:url" content={`https://acoha.site`} key="og-url" />
+        <meta property="og:image" content={acohaImage.src} key="og-image" />
+        <meta property="og:site_name" content="acoha" key="og-site" />
+      </Head>
       <body>
         <Main />
         <NextScript />
