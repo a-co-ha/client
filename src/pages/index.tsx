e7fc3a5a4dc8f8d5caed7a96f8f1227833f2c5f4
tangjinlog <lkg70007@gmail.com>
2023-05-24 02:53:41 +0900

    feat: intro Section ScrollEvent 구현 중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,9 @@ import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { IconOne } from '../components/project-sidebar/Icons';
+import Image from 'next/image';
+import commitLog from '@/images/landingPage/commitLog.png';
+import chat from '@/images/landingPage/chat.svg';
 
 const IndexPage = () => {
   const IntroSection = useRef<any>(null);
@@ -28,6 +31,9 @@ const IndexPage = () => {
   const messageD = useRef<HTMLDivElement>(null);
   const messageBackground = useRef<HTMLDivElement>(null);
   const introMonitorBox = useRef<HTMLDivElement>(null);
+  const introChatImgBox = useRef<HTMLDivElement>(null);
+  const introChatImg = useRef<HTMLImageElement>(null);
+  const introChatReplyBox = useRef<HTMLDivElement>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [clickItem, setClickItem] = useState('');
   console.log(`위messageA`, messageA);
@@ -53,7 +59,7 @@ const IndexPage = () => {
       }, 30),
     []
   );
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.addEventListener(`load`, () => {
       setIsLoading(false);
     });
@@ -79,8 +85,8 @@ const IndexPage = () => {
 
   const introClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     const target = e.currentTarget as HTMLDivElement;
-    const targetValue = target.textContent;
-    if (targetValue !== null) {
+    const targetValue = target.firstElementChild?.textContent;
+    if (targetValue !== undefined && targetValue !== null) {
       console.log(`targetValue`, targetValue === '기록');
       targetValue === clickItem ? setClickItem('') : setClickItem(targetValue);
     }
@@ -102,21 +108,29 @@ const IndexPage = () => {
         messageC: messageC,
         messageD: messageD,
         messageBackground: messageBackground,
-        // introMonitorBox: introMonitorBox,
+        introMonitorBox: introMonitorBox,
+        introChatImgBox: introChatImgBox,
+        introChatImg: introChatImg,
+        introChatReplyBox: introChatReplyBox,
       },
       values: {
-        messageBox_translateY_in: [-50, -250, { start: 0.4, end: 0.6 }],
         messageBox_colorR_in: [255, 0, { start: 0.05, end: 0.3 }],
         messageBox_colorG_in: [150, 0, { start: 0.05, end: 0.3 }],
         messageBox_colorB_in: [166, 0, { start: 0.05, end: 0.3 }],
-        messageBackground_translateY_in: [100, 0, { start: 0.2, end: 0.3 }],
+        messageBox_translateX_in: [0, 35, { start: 0.05, end: 0.6 }],
         messageB_translateX_in: [-100, 0, { start: 0.05, end: 0.3 }],
         messageC_translateX_in: [-100, 0, { start: 0.05, end: 0.3 }],
         messageD_translateX_in: [-80, 0, { start: 0.05, end: 0.3 }],
+        introChatImg_translateX_in: [0, -40, { start: 0.22, end: 0.6 }],
+        messageBackground_translateY_in: [100, 0, { start: 0.2, end: 0.3 }],
+        messageBox_translateY_in: [-50, 160, { start: 0.4, end: 0.6 }],
         messageA_translateY_in: [0, -40, { start: 0.4, end: 0.6 }],
         messageB_translateY_in: [0, -40, { start: 0.4, end: 0.6 }],
         messageC_translateY_in: [0, -40, { start: 0.4, end: 0.6 }],
         messageD_translateY_in: [0, -40, { start: 0.4, end: 0.6 }],
+        introChatImgBox_translateY_in: [50, 15, { start: 0.22, end: 0.4 }],
+        introChatImg_translateY_in: [25, -20, { start: 0.22, end: 0.6 }],
+        introChatReplyBox_translateY_in: [25, 0, { start: 0.65, end: 0.75 }],
         // introMonitorBox_translateY_in: [-50, -100, { start: 0.05, end: 0.3 }],
         // messageB_translateX_out: [0, -100, { start: 0.05, end: 0.3 }],
         // messageC_translateX_out: [0, -100, { start: 0.05, end: 0.3 }],
@@ -129,9 +143,14 @@ const IndexPage = () => {
         messageBackground_opacity_in: [0, 1, { start: 0.2, end: 0.3 }],
         messageB_opacity_in: [0, 1, { start: 0.15, end: 0.3 }],
         messageD_opacity_in: [0, 1, { start: 0.15, end: 0.3 }],
+        introMonitorBox_opacity_out: [1, 0, { start: 0.05, end: 0.2 }],
         messageB_opacity_out: [1, 0, { start: 0.4, end: 0.6 }],
         messageD_opacity_out: [1, 0, { start: 0.4, end: 0.6 }],
+        introChatImgBox_opacity_in: [0, 1, { start: 0.22, end: 0.3 }],
+        introChatReplyBox_opacity_in: [0, 1, { start: 0.65, end: 0.75 }],
+        // introChatImgBox_opacity_out: [1, 0, { start: 0.05, end: 0.2 }],
         messageBox_scale_in: [1, 0.7, { start: 0.4, end: 0.6 }],
+        introChatImg_width_in: [150, 250, { start: 0.42, end: 0.6 }],
       },
     },
   ];
@@ -142,9 +161,9 @@ const IndexPage = () => {
         sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       }
       if (sceneInfo[i].objs.container) {
-        sceneInfo[
-          i
-        ].objs.container.current.style.height = `${sceneInfo[i].scrollHeight}px`;
+        sceneInfo[i].objs.container.current.style.height = `${
+          sceneInfo[i].scrollHeight + window.innerHeight
+        }px`;
       }
     }
     // yOffset = window.pageYOffset;
@@ -211,12 +230,18 @@ const IndexPage = () => {
           objs.messageA.current &&
           objs.messageB.current &&
           objs.messageC.current &&
-          objs.messageD.current
-          // objs.introMonitorBox.current
+          objs.messageD.current &&
+          objs.introMonitorBox.current &&
+          objs.introChatImgBox.current &&
+          objs.introChatImg.current &&
+          objs.introChatReplyBox.current
         ) {
+          console.log(`scroll`, scrollRatio);
           if (scrollRatio <= 0.62) {
-            console.log(`y`, objs.messageA.current);
-            objs.messageBox.current.style.transform = `translate3d(0,${calcValues(
+            objs.messageBox.current.style.transform = `translate3d(${calcValues(
+              values.messageBox_translateX_in,
+              currentYOffset
+            )}%,${calcValues(
               values.messageBox_translateY_in,
               currentYOffset
             )}%,0) scale(${calcValues(
@@ -242,6 +267,11 @@ const IndexPage = () => {
               values.messageBackground_opacity_in,
               currentYOffset
             )}`;
+            objs.introMonitorBox.current.style.userSelect = `auto`;
+            objs.introMonitorBox.current.style.opacity = `${calcValues(
+              values.introMonitorBox_opacity_out,
+              currentYOffset
+            )}`;
             objs.messageA.current.style.transform = `translate3d(0, 0, 0)`;
             objs.messageB.current.style.transform = `translate3d(${calcValues(
               values.messageB_translateX_in,
@@ -263,42 +293,50 @@ const IndexPage = () => {
               values.messageD_opacity_in,
               currentYOffset
             )}`;
+            if (scrollRatio >= 0.22) {
+              objs.introChatImgBox.current.style.display = `block`;
+            } else {
+              objs.introChatImgBox.current.style.display = `none`;
+            }
+            objs.introChatImgBox.current.style.transform = `translate3d(0,${calcValues(
+              values.introChatImgBox_translateY_in,
+              currentYOffset
+            )}%,0)`;
+            objs.introChatImgBox.current.style.opacity = `${calcValues(
+              values.introChatImgBox_opacity_in,
+              currentYOffset
+            )}`;
+            objs.introChatImg.current.style.transform = `translate3d(${calcValues(
+              values.introChatImg_translateX_in,
+              currentYOffset
+            )}%,${calcValues(
+              values.introChatImg_translateY_in,
+              currentYOffset
+            )}%,0)`;
+            objs.introChatImg.current.style.width = `${calcValues(
+              values.introChatImg_width_in,
+              currentYOffset
+            )}%`;
+
             // objs.introMonitorBox.current.style.transform = `translate3d(0,${calcValues(
             //   values.introMonitorBox_translateY_in,
             //   currentYOffset
             // )}%, 0)`;
             // in
+          } else if (scrollRatio >= 0.22) {
+            objs.introMonitorBox.current.style.userSelect = `none`;
           }
-          // } else {
-          //   // out
-          //   objs.messageA.current.style.transform = `translate3d(0, 0, 0)`;
-          //   objs.messageB.current.style.transform = `translate3d(${calcValues(
-          //     values.messageB_translateX_out,
-          //     currentYOffset
-          //   )}%, ${calcValues(
-          //     values.messageB_translateY_out,
-          //     currentYOffset
-          //   )}%, 0)`;
-          //   // objs.messageB.current.style.opacity = `${calcValues(
-          //   //   values.messageB_opacity_out,
-          //   //   currentYOffset
-          //   // )}`;
-          //   objs.messageC.current.style.transform = `translate3d(${calcValues(
-          //     values.messageC_translateX_out,
-          //     currentYOffset
-          //   )}%, ${calcValues(
-          //     values.messageC_translateY_out,
-          //     currentYOffset
-          //   )}%, 0)`;
-          //     currentYOffset
-          //   )}%, ${calcValues(
-          //     currentYOffset
-          //   )}%, 0)`;
-          //   //   currentYOffset
-          //   // )}`;
-          // }
+          if (scrollRatio <= 0.77) {
+            objs.introChatReplyBox.current.style.transform = `translate3d(0,${calcValues(
+              values.introChatReplyBox_translateY_in,
+              currentYOffset
+            )}%,0)`;
+            objs.introChatReplyBox.current.style.opacity = `${calcValues(
+              values.introChatReplyBox_opacity_in,
+              currentYOffset
+            )}`;
+          }
         }
-
         // 	if (scrollRatio <= 0.42) {
         // 		// in
         // 		objs.messageB.style.opacity = calcValues(values.messageB_opacity_in, currentYOffset);
@@ -366,55 +404,100 @@ const IndexPage = () => {
               </div>
               <div ref={messageBackground} css={styles.messageBackground}></div>
             </div>
-            <div css={styles.introMonitorBox}>
-              <div
-                css={styles.introMonitorItem(clickItem === `기록`, clickItem)}
-                onClick={introClickHandler}
-              >
-                <FontAwesomeIcon
-                  icon={faPenToSquare}
-                  css={styles.introMonitorItemSvg}
-                />
-                기록
+            <div ref={introMonitorBox}>
+              <div css={styles.introMonitorBox}>
+                <div
+                  css={styles.introMonitorItem(clickItem === `기록`, clickItem)}
+                  onClick={introClickHandler}
+                >
+                  <div css={styles.introMonitorItemSvgBox}>
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      css={styles.introMonitorItemSvg}
+                    />
+                    기록
+                  </div>
+                  <div
+                    css={styles.introMonitorItemSvgText(clickItem === `기록`)}
+                  >{`프로젝트의 아이디어 공유나\n 희의록을 공유할 수 있어요`}</div>
+                </div>
+                <div
+                  css={styles.introMonitorItem(clickItem === `채팅`, clickItem)}
+                  onClick={introClickHandler}
+                >
+                  <div css={styles.introMonitorItemSvgBox}>
+                    <FontAwesomeIcon
+                      icon={faComments}
+                      css={styles.introMonitorItemSvg}
+                    />
+                    채팅
+                  </div>
+                  <div
+                    css={styles.introMonitorItemSvgText(clickItem === `채팅`)}
+                  >{`팀원들과 실시간 채팅으로 소통할 수 있어요`}</div>
+                </div>
+                <div
+                  css={styles.introMonitorItem(
+                    clickItem === `커밋로그`,
+                    clickItem
+                  )}
+                  onClick={introClickHandler}
+                >
+                  <div css={styles.introMonitorItemSvgBox}>
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      css={styles.introMonitorItemSvg}
+                    />
+                    커밋로그
+                  </div>
+                  <div
+                    css={styles.introMonitorItemSvgText(
+                      clickItem === `커밋로그`
+                    )}
+                  >{`프로젝트를 깃허브 저장소와 연결해서\n 커밋로그 및 이슈를 확인할 수 있어요`}</div>
+                </div>
+                <div
+                  css={styles.introMonitorItem(
+                    clickItem === `진행상황`,
+                    clickItem
+                  )}
+                  onClick={introClickHandler}
+                >
+                  <div css={styles.introMonitorItemSvgBox}>
+                    <FontAwesomeIcon
+                      icon={faListCheck}
+                      css={styles.introMonitorItemSvg}
+                    />
+                    진행상황
+                  </div>
+                  <div
+                    css={styles.introMonitorItemSvgText(
+                      clickItem === `진행상황`
+                    )}
+                  >{`체크리스트를 만들어 진행상황을 %로 확인할 수 있어요`}</div>
+                </div>
+                <div css={styles.arrowDownSvg}></div>
               </div>
-              <div
-                css={styles.introMonitorItem(clickItem === `채팅`, clickItem)}
-                onClick={introClickHandler}
-              >
-                <FontAwesomeIcon
-                  icon={faComments}
-                  css={styles.introMonitorItemSvg}
-                />
-                채팅
-              </div>
-              <div
-                css={styles.introMonitorItem(
-                  clickItem === `커밋로그`,
-                  clickItem
-                )}
-                onClick={introClickHandler}
-              >
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  css={styles.introMonitorItemSvg}
-                />
-                커밋로그
-              </div>
-              <div
-                css={styles.introMonitorItem(
-                  clickItem === `진행상황`,
-                  clickItem
-                )}
-                onClick={introClickHandler}
-              >
-                <FontAwesomeIcon
-                  icon={faListCheck}
-                  css={styles.introMonitorItemSvg}
-                />
-                진행상황
-              </div>
-              <div css={styles.arrowDownSvg}>
-                {/* <FontAwesomeIcon icon={faChevronDown} /> */}
+            </div>
+            <div ref={introChatImgBox} css={styles.introChatBox}>
+              <div css={styles.introChatBox}>
+                <div css={styles.introChatImgBox}>
+                  <Image
+                    ref={introChatImg}
+                    css={styles.introChatImg}
+                    src={chat}
+                    width={300}
+                    height={200}
+                    alt={`intro chat img`}
+                  />
+                  <div
+                    ref={introChatReplyBox}
+                    css={styles.introChatImgReplyBox}
+                  >
+                    <div>{`팀원을 초대하고\n프로젝트를 시작해 볼까요?`}</div>
+                  </div>
+                </div>
+                <div></div>
               </div>
             </div>
           </div>
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -76,11 +76,36 @@ export const messageBox = css`
   color: #ffd6dc;
   font-size: 4rem;
   font-weight: 700;
+  z-index: 1;
   white-space: nowrap;
   opacity: 0;
   animation: ${mainTitleAni} 0.7s 2.3s forwards;
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1280px) {
+    font-size: 3.8rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 1150px) {
+    font-size: 3.5rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 1024px) {
     font-size: 3rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 890px) {
+    font-size: 2.5rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 640px) {
+    font-size: 1.75rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 520px) {
+    font-size: 1.5rem;
     margin: 0;
   }
 `;
@@ -120,20 +145,9 @@ export const messageBackground = css`
   font-size: 0.75rem;
   font-weight: 500;
   background: #ffd6dc;
-  border-radius: 0.75rem;
+  border-radius: 1rem;
+  border-top-left-radius: 0.2rem;
   box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-  &::after {
-    position: absolute;
-    content: '';
-    background: #ffd6dc;
-    width: 15px;
-    height: 15px;
-    left: calc(50% - 7.5px);
-    bottom: -7.5px;
-    transform: rotate(45deg);
-    box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1),
-      0 4px 6px -4px rgb(0 0 0 / 0.1);
-  }
   opacity: 0;
   @media screen and (max-width: 768px) {
   }
@@ -152,7 +166,6 @@ export const introMonitorBox = css`
   border-radius: 8px;
   transform: translate3d(0, 0, 0);
   opacity: 0;
-  overflow: hidden;
   @media screen and (max-width: 768px) {
     width: 100%;
     height: 200px;
@@ -189,7 +202,7 @@ export const introMonitorBox = css`
 export const introMonitorItem = (isClicked: boolean, clickItem: string) => css`
   position: absolute;
   display: flex;
-  gap: 0.5rem;
+
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -200,7 +213,7 @@ export const introMonitorItem = (isClicked: boolean, clickItem: string) => css`
   background: white;
   border: 1px solid black;
   border-radius: 4px;
-  transition: 0.5s;
+  transition: 0.3s;
   transform: scale(${isClicked ? `2` : `1`});
   &:hover {
     color: rgba(255, 0, 0, 0.5);
@@ -209,17 +222,49 @@ export const introMonitorItem = (isClicked: boolean, clickItem: string) => css`
     font-size: 1.5rem;
     gap: 1rem;
   }
+  & > div:first-child {
+    position: absolute;
+    top: ${isClicked ? `20%` : `50%`};
+    left: ${isClicked ? `15%` : `50%`};
+    transition: 0.5s;
+    transform: translate3d(-50%, -50%, 0) scale(${isClicked ? `0.5` : `1`});
+  }
 `;
+
+export const introMonitorItemSvgBox = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+`;
+
 export const introMonitorItemSvg = css`
   font-size: 4rem;
   @media screen and (min-width: 1024px) {
-    font-size: 6rem;
+    font-size: 5rem;
+  }
+  @media screen and (max-width: 1023px) {
+    font-size: 3.5rem;
   }
   @media screen and (max-width: 768px) {
     font-size: 2.5rem;
   }
 `;
 
+export const introMonitorItemSvgText = (isClicked: boolean) => css`
+  transition: 0.2s;
+  width: 80%;
+  margin-top: 1rem;
+  font-size: 1rem;
+  opacity: ${isClicked ? `1` : `0`};
+  @media screen and (max-width: 1024px) {
+    font-size: 0.8rem;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 0.6rem;
+  }
+`;
 export const arrowDownSvg = css`
   position: absolute;
   left: calc(50% - 20px);
@@ -242,4 +287,102 @@ export const arrowDownSvg = css`
     box-shadow: 3px -1px 5px 1px rgb(0 0 0 / 0.2);
   }
   box-shadow: 3px 1px 5px 1px rgb(0 0 0 / 0.2);
+`;
+
+export const introChatBox = css`
+  display: flex;
+  width: 100%;
+  height: 470px;
+  position: absolute;
+  @media screen and (max-width: 1280px) {
+    height: 470px;
+  }
+  @media screen and (max-width: 1024px) {
+    height: 400px;
+  }
+  @media screen and (max-width: 768px) {
+    height: 300px;
+  }
+  @media screen and (max-width: 520px) {
+    height: 270px;
+  }
+  &::before {
+    display: block;
+    position: absolute;
+    content: 'Chat';
+    top: -20px;
+    left: 0;
+    width: 100%;
+    height: 20px;
+    text-align: center;
+    font-weight: 600;
+    line-height: 20px;
+    color: rgba(255, 255, 255, 0.6);
+    background: rgba(0, 0, 0, 0.2);
+    border-top-left-radius: 0.375rem;
+    border-top-right-radius: 0.375rem;
+  }
+`;
+
+export const introChatInnerBox = css`
+  width: 100%;
+`;
+
+export const introChatImgBox = css`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-bottom-left-radius: 0.375rem;
+  border-bottom-right-radius: 0.375rem;
+  overflow: hidden;
+  box-shadow: 0 5px 5px -3px rgb(0 0 0 / 0.2), 0 4px 6px -4px rgb(0 0 0 / 0.2);
+`;
+
+export const introChatImg = css`
+  max-width: unset;
+  width: 150%;
+`;
+
+export const introChatImgReplyBox = css`
+  position: absolute;
+  bottom: 35%;
+  right: 7%;
+  width: 65%;
+  padding: 0.2rem 0 0.2rem 0.5rem;
+  font-size: 2.2rem;
+  font-weight: 600;
+  text-align: start;
+  background: #bddc95;
+  border-radius: 0.7rem;
+  border-top-right-radius: 0.2rem;
+  white-space: pre-wrap;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  @media screen and (max-width: 1280px) {
+    font-size: 2.1rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 1150px) {
+    font-size: 1.9rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 1024px) {
+    font-size: 1.5rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 890px) {
+    font-size: 1.35rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 1.1rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 640px) {
+    font-size: 1rem;
+    margin: 0;
+  }
+  @media screen and (max-width: 520px) {
+    font-size: 0.85rem;
+    margin: 0;
+  }
 `;
