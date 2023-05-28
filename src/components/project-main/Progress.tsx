385a1938dfe083d4a03cd4375397581b472ff0e5
ghkd1418 <1004wipi@gmail.com>
2023-05-28 13:15:18 +0900

    refactor: 프로젝트가 하나도 없을 시 프로젝트 생성 버튼 로직 분리

--- src/components/project-main/Progress.tsx
+++ src/components/project-main/Progress.tsx
@@ -3,7 +3,7 @@ import { useGetUrlInfo } from '@/hooks/useGetUrlInfo';
 import { ProgressGauge } from '../template/progressGauge';
 import * as styles from './styles';
 import Link from 'next/link';
-import { useCreateTemplate } from '@/hooks/queries/template/useCreateTemplate';
+import { CreateProgressTemplate } from './CreateProgressTemplate';
 
 export interface PageProgress {
   pageName: string;
@@ -14,11 +14,6 @@ export interface PageProgress {
 export const Progress = () => {
   const { channelId } = useGetUrlInfo();
   const { data: pagePercentList } = useGetProgressPercentList(channelId);
-  console.log(
-    '🚀 ~ file: Progress.tsx:16 ~ Progress ~ pagePercentList:',
-    pagePercentList
-  );
-  const { mutate: createTemplateMutate } = useCreateTemplate();
 
   return (
     <div css={styles.commonBoxStyle}>
@@ -40,27 +35,7 @@ export const Progress = () => {
             </div>
           ))
         ) : (
-          <button
-            css={styles.svgButtonStyles}
-            onClick={() => createTemplateMutate('progress')}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-              css={styles.svgIconStyles}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-              />
-            </svg>
-            <p>프로젝트 시작하기</p>
-          </button>
+          <CreateProgressTemplate />
         )}
       </main>
     </div>
