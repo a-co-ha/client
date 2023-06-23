5c7ebb40b1e2a604c32de367349148027d07cae8
ghkd1418 <1004wipi@gmail.com>
2023-06-23 15:27:46 +0900

    feat: title, og 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>a-coha</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://acoha.site/" />
+        <meta property="og:title" content="a-coha" />
+        <meta property="og:image" content={'/images/channelImg/9.png'} />
+        <meta property="og:description" content="프로젝트 관리 툴" />
+        <meta property="og:site_name" content="a-coha" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
       <body>
         <Main />
         <NextScript />
