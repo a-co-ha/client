fa539c2ec1f2458756e452894d09e54affbd005b
ghkd1418 <1004wipi@gmail.com>
2023-06-24 12:55:39 +0900

    style: RecentPosts 스타일 수정 및 아이콘 추가

--- src/components/project-main/RecentPosts.tsx
+++ src/components/project-main/RecentPosts.tsx
@@ -9,7 +9,23 @@ export const RecentPosts = () => {
 
   return (
     <section css={Container}>
-      <h2>✨최근 수정된 게시물</h2>
+      <b css={Title}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+        최근 수정된 게시물
+      </b>
       <ul>
         {data.map(({ pageName, type, updatedAt, _id }) => {
           return (
@@ -19,8 +35,7 @@ export const RecentPosts = () => {
               >
                 <p css={Content}>
                   {pageName}
-                  <br />
-                  {updatedAt}에 마지막으로 수정됨
+                  <span css={UpdateAtStyle}>{updatedAt}에 수정됨</span>
                 </p>
               </Link>
             </li>
@@ -33,20 +48,35 @@ export const RecentPosts = () => {
 
 const Container = css`
   display: flex;
-  border: solid;
   height: 20rem;
-  width: 30rem;
-  padding: 1rem;
+  width: 15rem;
   flex-direction: column;
+`;
+
+const Title = css`
+  display: flex;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  align-items: center;
+  background-color: #ffd6dc;
+  border-top-left-radius: 0.37rem;
+  border-top-right-radius: 0.37rem;
+  & svg {
+    width: 2rem;
+  }
+  margin-bottom: 0.5rem;
 `;
 
 const Content = css`
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  padding: 8px 10px 6px 6px;
+  padding: 0.5rem;
   border-radius: 5px;
   margin-bottom: 0.5rem;
   display: flex;
   flex-direction: column;
-  height: auto;
-  min-height: 3.5rem;
+`;
+
+const UpdateAtStyle = css`
+  font-size: 0.8rem;
+  color: gray;
 `;
