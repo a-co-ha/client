66fb69b4a2015f9ccdbcee652e2b51e733b1eefa
ghkd1418 <1004wipi@gmail.com>
2023-07-02 17:20:00 +0900

    style: NoticeDetail content 스크롤 추가

--- src/components/project-main/Notice/NoticeDetail.tsx
+++ src/components/project-main/Notice/NoticeDetail.tsx
@@ -70,6 +70,23 @@ const Content = css`
   flex-direction: column;
   gap: 1rem;
   word-wrap: break-word;
+
+  overflow: hidden scroll;
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-clip: padding-box;
+    border: 5px solid transparent;
+    background-color: #efefef;
+    border-radius: 0.5rem;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.4);
+    }
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
 `;
 
 const Title = css`
